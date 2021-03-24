41ee6b6a23ad13a48dc33c056683b88c2363e909
shlomota <shlomotannor@mail.tau.ac.il>
2021-03-24 17:59:11 +0200

    changed to nova

--- trainBM.py
+++ trainBM.py
@@ -1,12 +1,9 @@
 from tell.commands.train import train_model_from_file
 import shutil
-import os
 
 base_path = "/a/home/cc/students/cs/shlomotannor/nlp_course/newscaptioning/"
 parameter_filename = "expt/nytimes/BM/config.yaml"
 serialization_dir = base_path + "expt/nytimes/BM/serialization_sum"
 
-print("directory content:", os.listdir(serialization_dir))
 shutil.rmtree(serialization_dir)
-print("after remove")
 train_model_from_file(parameter_filename, serialization_dir)
