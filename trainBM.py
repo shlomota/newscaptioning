575faa45a4d298ef2db0ee95cc572d8e6ff8d2b8
shlomota <shlomotannor@mail.tau.ac.il>
2021-03-24 14:34:31 +0200

    updated serialization path trainBM

--- trainBM.py
+++ trainBM.py
@@ -3,7 +3,7 @@ import shutil
 
 base_path = "/a/home/cc/students/cs/shlomotannor/nlp_course/newscaptioning/"
 parameter_filename = "expt/nytimes/BM/config.yaml"
-serialization_dir = base_path + "expt/nytimes/BM/serialization"
+serialization_dir = base_path + "expt/nytimes/BM/serialization_sum"
 
 shutil.rmtree(serialization_dir)
 train_model_from_file(parameter_filename, serialization_dir)
