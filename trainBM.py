7e5818365358314ae38047e1839d89437d2eaeb0
shlomota <shlomotannor@mail.tau.ac.il>
2021-03-24 16:10:41 +0200

    remove rmdir

--- trainBM.py
+++ trainBM.py
@@ -5,5 +5,5 @@ base_path = "/a/home/cc/students/cs/shlomotannor/nlp_course/newscaptioning/"
 parameter_filename = "expt/nytimes/BM/config.yaml"
 serialization_dir = base_path + "expt/nytimes/BM/serialization_sum"
 
-shutil.rmtree(serialization_dir)
+# shutil.rmtree(serialization_dir)
 train_model_from_file(parameter_filename, serialization_dir)
