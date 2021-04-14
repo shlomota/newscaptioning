48b9fedf97dcff58e6adbbfd554dfe0974dfbe0b
shlomota <shlomotannor@mail.tau.ac.il>
2021-04-15 01:32:43 +0300

    restored simple mean

--- train_rel.py
+++ train_rel.py
@@ -4,9 +4,9 @@ import os
 
 base_path = "/a/home/cc/students/cs/shlomotannor/nlp_course/newscaptioning/"
 parameter_filename = "expt/nytimes/BMRel/config.yaml"
-serialization_dir = base_path + "expt/nytimes/BMRel/serialization"
+serialization_dir = base_path + "expt/nytimes/BMRel/serialization_mean"
 
-LOSSLOG = os.path.join(base_path, 'BM_rel.log')
+LOSSLOG = os.path.join(base_path, 'BM_rel2.log')
 
 if os.path.exists(serialization_dir):
     shutil.rmtree(serialization_dir)
