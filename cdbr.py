075533c11a82f4f3f889ab9f9560a47fec7dfb2f
Maor Kehati <maorkehati@rack-wolfson-g01.cs.tau.ac.il>
2021-04-15 01:50:29 +0300

    c

--- cdbr.py
+++ cdbr.py
@@ -1,4 +1,7 @@
 from glob import glob
+import time
 
-print(f'train: {len(glob("dbr/*[!m]"))-2}, test: {len(glob("dbr/test/*[!m]"))-1}')
+while True:
+    print(f'{time.ctime()}: train: {len(glob("dbr/*[!m]"))-4}, test: {len(glob("dbr/test/*[!m]"))-1}')
+    time.sleep(120)
 
