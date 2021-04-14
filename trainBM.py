87ba0e1affd4eb23a7d8ee5246a15cc1e03c9525
shlomota <shlomotannor@mail.tau.ac.il>
2021-04-15 00:17:12 +0300

    train BM 40

--- trainBM.py
+++ trainBM.py
@@ -4,7 +4,7 @@ import shutil
 
 base_path = "/a/home/cc/students/cs/shlomotannor/nlp_course/newscaptioning/"
 parameter_filename = "expt/nytimes/BM/config.yaml"
-serialization_dir = base_path + "expt/nytimes/BM/serialization_test"
+serialization_dir = base_path + "expt/nytimes/BM/serialization_40"
 
 if os.path.exists(serialization_dir):
     shutil.rmtree(serialization_dir)
