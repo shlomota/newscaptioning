3d178e48136fb86823ae8c1913f2f73a655cf080
shlomota <shlomotannor@mail.tau.ac.il>
2021-04-18 02:22:44 +0300

    updated configs

--- tell/models/BMRelModel.py
+++ tell/models/BMRelModel.py
@@ -132,8 +132,9 @@ class BMRelModel(Model):
         # hiddens = torch.stack([self.roberta.extract_features(p).detach() for p in c])  # [B, K, N, 1024]
 
         hiddens = [torch.load(f"{self.dbr}{dbrf}/{i}") for i in aid]
-        masks = [torch.load(f"{self.dbr}{dbrf}/{i}m") for i in aid]
+        hiddens = torch.stack(hiddens)
         hiddens = [hiddens[:, [index1[i], index2[i]], :] for i in range(len(index1))]
+        masks = [torch.load(f"{self.dbr}{dbrf}/{i}m") for i in aid]
         masks = [masks[:, [index1[i], index2[i]], :] for i in range(len(index1))]
         m = [torch.add(i.unsqueeze(-1).expand(*i.shape, 1024), 1) for i in masks]
         # cshape = c.shape
