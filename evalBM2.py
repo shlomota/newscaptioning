90e567821c65563c978a972610d4fe0e7d8a39a4
Shlomo Tannor <shlomotannor@rack-wolfson-g01.cs.tau.ac.il>
2021-04-20 12:47:26 +0300

    ada

---
+++ evalBM2.py
@@ -0,0 +1,26 @@
+import os
+import datetime
+from tell.commands.evaluate import evaluate_from_file as eff
+import shutil
+
+TAT_FOLDER = "/specific/netapp5/joberant/nlp_fall_2021/shlomotannor/newscaptioning/"
+OUTS_FOLDER = "outs"
+model = "16_bm_sm_model"
+
+YAML = f"expt/nytimes/{model}/config.yaml"
+YAML = os.path.join(TAT_FOLDER, YAML)
+# expt/nytimes/BM/serialization_sum_good/best.th
+SER = f"expt/nytimes/{model}/serialization/best.th"
+# SER = "expt/nytimes/{}/serialization_sum_good/best.th".format(model)
+SER = os.path.join(TAT_FOLDER, SER)
+
+G_JSONL = f"expt/nytimes/{model}/serialization/generations.jsonl"
+G_JSONL = os.path.join(TAT_FOLDER, G_JSONL)
+
+# if os.path.exists(G_JSONL):
+#     os.remove(G_JSONL)
+eff(YAML, SER, device=0)
+
+now = datetime.datetime.now()
+shutil.copyfile(G_JSONL, os.path.join(TAT_FOLDER, OUTS_FOLDER,
+                                      f"generations_{now.year}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.jsonl"))
